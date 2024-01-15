7d63cdddeecc51869f730ab2b1a15783a68d924e
anjali-1607 <anjalirajpoot1609@gmail.com>
2024-01-15 23:45:05 +0530

    tagline

--- src/Components/TagLine/Tagline.jsx
+++ src/Components/TagLine/Tagline.jsx
@@ -4,7 +4,7 @@ export default function Tagline() {
   return (
     <>
       <div className="row mt-4 mb-4">
-        <h1 className="dancing-curls alignCentre display-4 ">
+        <h1 className="Jost sans-serif alignCentre display-4 ">
           "Empowering Communities, Inspiring Change."
         </h1>
       </div>
